8d319fff66cf1e7d1a1ef15771d65c661a72e232
shamanov-d <barbozyaks@gmail.com>
2022-11-01 15:20:14 +0300

    учет заблакированных

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { CallbackService, Objects, VK } from "vk-io";
+import {CallbackService, Objects, VK} from "vk-io";
 import {
   DirectAuthorization,
   officialAppCredentials,
 } from "@vk-io/authorization";
 import readlineSync from "readline-sync";
-import { Command } from "commander";
-import { Storage } from "./storage";
+import {Command} from "commander";
+import {Storage} from "./storage";
 import CaptchaResolver from "rucaptcha-api";
 
 // colors
@@ -20,8 +20,11 @@ interface Options {
 
 const apiVersion = "5.154";
 
-
-const loginUser = async (data: { token: string; user: number }, arg?: string, name?: string) => {
+const loginUser = async (
+  data: {token: string; user: number},
+  arg?: string,
+  name?: string,
+) => {
   if (!data) {
     let password: string;
     let login: string;
@@ -30,8 +33,10 @@ const loginUser = async (data: { token: string; user: number }, arg?: string, na
       if (logPass.length === 2) {
         [login, password] = logPass;
       } else {
-        const user = await new VK({ token: arg, apiVersion }).api.account.getProfileInfo({}).then(({ id }) => id);
-        return { token: arg, user };
+        const user = await new VK({token: arg, apiVersion}).api.account
+          .getProfileInfo({})
+          .then(({id}) => id);
+        return {token: arg, user};
       }
     } else {
       console.log(`auth ${name}`);
@@ -39,7 +44,7 @@ const loginUser = async (data: { token: string; user: number }, arg?: string, na
       password = readlineSync.question("password:").trim();
     }
     const callbackService = new CallbackService();
-    callbackService.onTwoFactor(async ({ phoneMask, type }, retry) => {
+    callbackService.onTwoFactor(async ({phoneMask, type}, retry) => {
       console.log(`send two-factor code type:${type} phone:${phoneMask}`);
       await retry(readlineSync.question("code:").trim());
     });
@@ -57,11 +62,10 @@ const loginUser = async (data: { token: string; user: number }, arg?: string, na
       apiVersion,
     });
     console.log("authorization start...");
-    const { token, user } = await direct.run();
-    return { token, user };
+    const {token, user} = await direct.run();
+    return {token, user};
   } else return data;
-}
-
+};
 
 const cliApp = new Command();
 cliApp
@@ -69,12 +73,19 @@ cliApp
   .option("-su, --sourceUser [string]", "password vk page")
   .option("-tu, --targetUser [string]", "login vk page")
   .option("-ex, --exclude [string]", "login vk page")
-  .action(async (_, { sourceUser, targetUser, exclude }: Options) => {
-
-    let storage = Storage.get();
-    storage.sourceUser = await loginUser(storage.sourceUser, sourceUser, "sourceUser");
+  .action(async (_, {sourceUser, targetUser, exclude}: Options) => {
+    const storage = Storage.get();
+    storage.sourceUser = await loginUser(
+      storage.sourceUser,
+      sourceUser,
+      "sourceUser",
+    );
     Storage.save(storage);
-    storage.targetUser = await loginUser(storage.targetUser, targetUser, "targetUser");
+    storage.targetUser = await loginUser(
+      storage.targetUser,
+      targetUser,
+      "targetUser",
+    );
     const excludeList = exclude?.split(",")?.map(Number)?.filter(Boolean);
     Storage.save(storage);
     if (excludeList?.length) storage.sourceUser.excludePage = excludeList;
@@ -83,79 +94,113 @@ cliApp
       Storage.save(storage);
     }
 
-
-
-    const vkSource = new VK({ token: storage.sourceUser.token, apiVersion });
-    const vkTarget = new VK({ token: storage.targetUser.token, apiVersion });
+    const vkSource = new VK({token: storage.sourceUser.token, apiVersion});
+    const vkTarget = new VK({token: storage.targetUser.token, apiVersion});
     const captchaResolver = new CaptchaResolver({
       key: storage.captchaToken,
       callbackNullBalance: () => "монета кончилась на счету",
     });
 
     vkSource.callbackService.onCaptcha((ret, retry) => {
-      captchaResolver.resolveNormalCaptcha({ url: ret.src })
-        .then(asd => { retry(asd!) })
-    })
+      captchaResolver.resolveNormalCaptcha({url: ret.src}).then(asd => {
+        retry(asd!);
+      });
+    });
     vkTarget.callbackService.onCaptcha((ret, retry) => {
-      captchaResolver.resolveNormalCaptcha({ url: ret.src })
-        .then(asd => { retry(asd!) })
-    })
+      captchaResolver.resolveNormalCaptcha({url: ret.src}).then(asd => {
+        retry(asd!);
+      });
+    });
 
-
-    type PageT = Objects.GroupsGroupFull & { is_closed: number };
-    const list = await vkSource.api.groups.get({
+    type PageT = Objects.GroupsGroupFull & {is_closed: number};
+    const list = (await vkSource.api.groups.get({
       filter: ["admin", "editor"],
-      extended: true
-    }) as unknown as {
+      extended: true,
+    })) as unknown as {
       items: PageT[];
-    }
+    };
 
-    console.log(`Администрируемых сообществ в источниках: ${list.items.length}`);
-    const publicId = list.items.filter(({ is_closed }) => is_closed === 0);
-    console.log(`Администрируемых публичных сообществ в источниках: ${list.items.length}`);
+    console.log(
+      `Администрируемых сообществ в источниках: ${list.items.length}`,
+    );
+    const publicPage = list.items.filter(
+      ({is_closed, deactivated}) => is_closed === 0 && !Boolean(deactivated),
+    );
+    const banned = list.items.filter(({deactivated}) => Boolean(deactivated));
+    console.log(
+      `Администрируемых публичных сообществ в источниках: ${list.items.length}`,
+    );
+
+    console.log(
+      `Заблокированных сообществ в источнике(${storage.sourceUser.user}): ${banned.length}`,
+    );
+    banned.map(page => {
+      console.log(`${page.deactivated}\t (${page.id})\t${page.name}`);
+    });
 
     let offset;
-    let AllTargetPage = new Set<number>();
+    const AllTargetPage = new Set<number>();
     let items: number[] = [];
     do {
-      items = await vkTarget.api.groups.get({
-        offset,
-        count: 1000
-      }).then(({ items }) => items);
-      items.map(id => AllTargetPage.add(id))
+      items = await vkTarget.api.groups
+        .get({
+          offset,
+          count: 1000,
+        })
+        .then(({items}) => items);
+      items.map(id => AllTargetPage.add(id));
     } while (items.length === 1000);
 
-    const addedId = publicId.filter(({ id }) => !AllTargetPage.has(id!)).map(({ id }) => id);
+    const addedId = publicPage
+      .filter(({id}) => !AllTargetPage.has(id!))
+      .map(({id}) => id);
     console.log(`Сообщества в которые необходимо вступить ${addedId.length}`);
 
-    const pageCache = publicId.reduce((acc, page) => {
+    const pageCache = publicPage.reduce((acc, page) => {
       return {
         ...acc,
-        [page.id!]: page
+        [page.id!]: page,
       };
-    }, {} as { [key: string]: PageT })
+    }, {} as {[key: string]: PageT});
 
+    let addedCount = 0;
     await Promise.all(
-      addedId.map(async (id) => {
+      addedId.map(async id => {
         const page = pageCache[id!];
-        console.log(`Вступили в страницу (${page.id})\t${page.name}`);
-        await vkTarget.api.groups.join({
-          group_id: id
-        }).catch(err => console.log(id, err));
-      }));
+        await vkTarget.api.groups
+          .join({
+            group_id: id,
+          })
+          .then(() =>
+            console.log(
+              `Вступили в страницу (${page.id})(${addedCount++}/${
+                addedId.length
+              })\t${page.name}`,
+            ),
+          )
+          .catch(err => console.log(id, err));
+      }),
+    );
 
+    let publicCount = 0;
     await Promise.all(
-      publicId.map(async ({ id }) => {
+      publicPage.map(async ({id}) => {
         const page = pageCache[id!];
-        vkSource
-          .api.groups.editManager({
+        vkSource.api.groups
+          .editManager({
             group_id: id!,
             user_id: storage.targetUser.user,
-            role: "administrator"
+            role: "administrator",
           })
-          .then(ret => console.log(`${ret ? "Назначили админов" : "Уже администратор"} (${page.id})\t${page.name}`))
-          .catch(err => console.log(id, err))
-      })
-    )
+          .then(ret =>
+            console.log(
+              `${ret ? "Назначили админов" : "Уже администратор"} (${
+                page.id
+              })(${publicCount++}/${publicPage.length})\t${page.name}`,
+            ),
+          )
+          .catch(err => console.log(id, err));
+      }),
+    );
   });
 cliApp.parse();
